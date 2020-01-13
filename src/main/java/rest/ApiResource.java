4bf04cc688ae5bca85353fa2f72cf1d14524051d
Camilla Staunstrup <camilla.valerius@gmail.com>
2020-01-13 21:08:32 +0100

    removed unused endpoints and tests

--- src/main/java/rest/ApiResource.java
+++ src/main/java/rest/ApiResource.java
@@ -38,19 +38,11 @@ public class ApiResource {
         return "{\"msg\":\"Hello anonymous\"}";
     }
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("five")
-    @RolesAllowed({"admin", "user"})
-    public Map getDataFrom5Endpoints() throws Exception {
-        return facade.allApiData();
-    }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("all")
     public String allUsers() {
-
         EntityManager em = EMF.createEntityManager();
         try {
             List<User> users = em.createQuery("select user from User user").getResultList();
@@ -78,37 +70,6 @@ public class ApiResource {
         return "{\"msg\": \"Hello to (admin) User: " + thisuser + "\"}";
     }
 
-    /**
-     * The idea is here, that this endpoint is only available to registered
-     * users (and admins) While the allUsers (/all) endpoint is available to
-     * anyone
-     *
-     * @return
-     */
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("both")
-    @RolesAllowed({"admin", "user"})
-    public String getMultipleRoles() {
-        String thisuser = securityContext.getUserPrincipal().getName();
-        return "{\"msg\": \"Hello to (admin OR user, but not a nobody) User: " + thisuser + "\"}";
-    }
-
-    /**
-     * DEPRECATED: DONT THINK THIS IS POSSIBLE. Only accessible by a super-user
-     * (that holds both admin & user)
-     *
-     * @return
-     */
-    @Deprecated
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("bothrestricted")
-    @RolesAllowed(value = {"admin, user"})
-    public String getBothRoles() {
-        String thisuser = securityContext.getUserPrincipal().getName();
-        return "{\"msg\": \"Hello to (superuser) User: " + thisuser + "\"}";
-    }
 
     @GET
     @Path("testdata")
--- src/test/java/facades/UserFacadeTest.java
+++ src/test/java/facades/UserFacadeTest.java
@@ -19,23 +19,19 @@ import org.junit.jupiter.api.Test;
 import utils.EMF_Creator.DbSelector;
 import utils.EMF_Creator.Strategy;
 
-//Uncomment the line below, to temporarily disable this test
 //@Disabled
 public class UserFacadeTest {
 
     private static EntityManagerFactory emf;
     private static UserFacade facade;
 
-    // USERS
     private static User user;
     private static User admin;
     private static User both;
 
-    // ROLES
     private static Role userRole;
     private static Role adminRole;
 
-    // NON-HASHED PASSWORDS
     private static final String userPass = "user";
     private static final String adminPass = "admin";
     private static final String bothPass = "both";
@@ -45,37 +41,25 @@ public class UserFacadeTest {
 
     @BeforeAll
     public static void setUpClass() {
-        // SET UP CONNECTION AND FACADE
         emf = EMF_Creator.createEntityManagerFactory(DbSelector.TEST, Strategy.DROP_AND_CREATE);
         facade = UserFacade.getUserFacade(emf);
 
-        // SET UP USERS
         user = new User("user", userPass);
         admin = new User("admin", adminPass);
         both = new User("both", bothPass);
 
-        // SET UP ROLES
         userRole = new Role("user");
         adminRole = new Role("admin");
 
-        // ADD ROLES.
         user.addRole(userRole);
         admin.addRole(adminRole);
         both.addRole(userRole);
         both.addRole(adminRole);
     }
 
-    @AfterAll
-    public static void tearDownClass() {
-        // Clean up database after test is done or use a persistence unit with drop-and-create to start up clean on every test
-    }
-
-    // Setup the DataBase in a known state BEFORE EACH TEST
-    // TODO -- Make sure to change the script below to use YOUR OWN entity class
     @BeforeEach
     public void setUp() {
         EntityManager em = emf.createEntityManager();
-        // ADD ENTITIES TO DATABASE BEFORE EACH TEST 
         em.getTransaction().begin();
         em.persist(userRole);
         em.persist(adminRole);
@@ -92,7 +76,6 @@ public class UserFacadeTest {
 
     @AfterEach
     public void tearDown() {
-        // Remove any data after each test was run
         EntityManager em = emf.createEntityManager();
         try {
             em.getTransaction().begin();
@@ -114,28 +97,17 @@ public class UserFacadeTest {
 
     @Test
     public void testVerifyUsers() throws AuthenticationException {
-        // Arrange 
         User expected = user;
-        // Act
         String username = expected.getUserName();
         User actual = facade.getVeryfiedUser(username, userPass);
-        // Assert
         assertEquals(expected, actual);
-
-        // Repeat for Admin
         expected = admin;
-        // Act
         username = expected.getUserName();
         actual = facade.getVeryfiedUser(username, adminPass);
-        // Assert
         assertEquals(expected, actual);
-
-        // Repeat for Both
         expected = both;
-        // Act
         username = expected.getUserName();
         actual = facade.getVeryfiedUser(username, bothPass);
-        // Assert
         assertEquals(expected, actual);
     }
 
