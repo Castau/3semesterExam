9146903d48440f59ec0c4b2ea8b192657f66e26a
Camilla Staunstrup <camilla.valerius@gmail.com>
2020-01-13 14:57:48 +0100

    welcome page and login page supplied with info

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
-import { Badge } from 'react-bootstrap';
+import React, { useState, Fragment } from "react";
+import facade from "../apiFacade";
+import { Badge, Card, Container, Row, CardGroup, CardColumns, ListGroup } from 'react-bootstrap';
 
 export const LogIn = ({ login, message }) => {
   const [user, setUser] = useState({ username: "", password: "" });
+  const [errorMessage, setErrorMessage] = useState('');
+  const [data, setData] = useState('');
 
   function log_in(evt) {
     evt.preventDefault();
@@ -13,21 +16,68 @@ export const LogIn = ({ login, message }) => {
     setUser({ ...user, [evt.target.id]: evt.target.value });
   };
 
+  const handleClick = () => {
+    facade
+      .fetchGetData('testdata', '')
+      .then(res => {
+        setData(res);
+        setErrorMessage('');
+      })
+      .catch(err => {
+        if (err.status) {
+          err.fullError.then(e => {
+            setErrorMessage(e.message);
+            console.log(e.code, e.message);
+          });
+        } else {
+          console.log('Network error');
+        }
+      });
+  };
+
   return (
     <div>
       <h3 className="mt-2"> <Badge variant="info">Please log in to use the application</Badge></h3>
       <h4> <Badge variant="danger">{message}</Badge></h4>
       <form onSubmit={log_in}>
         <input placeholder="User Name" id="username" onChange={onChange} />{" "}
-        <input
-          placeholder="Password"
-          id="password"
-          type="password"
-          onChange={onChange}
-        />{" "}
-        <button className="btn-dark">Login</button>
-        <br></br>
+        <input placeholder="Password" id="password" type="password" onChange={onChange} />
+        <button className="btn-dark mb-3">Login</button>
       </form>
+      <Container className="mt-4">
+        <Row >
+          <Card className="w-25">
+            <Card.Body>
+              <Card.Title><h5><Badge variant="info">Supply database with data</Badge></h5></Card.Title>
+              <Card.Text>
+                <button className="btn-dark mb-3" onClick={handleClick}>Generate data</button>
+                <p className="mt-3">{data.message} </p>
+                <h5><Badge variant="info">Test Users</Badge></h5>
+              </Card.Text>
+              <ListGroup className="about_link" variant="flush">
+                <ListGroup.Item>
+                  <p className="mb-0">User: admin</p><p className="mb-0"> Pass: admin</p>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <p className="mb-0">User: user</p><p className="mb-0"> Pass: user</p>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <p className="mb-0">User: karen77</p><p className="mb-0"> Pass: mortil3</p>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <p className="mb-0">User: vlad</p><p className="mb-0"> Pass: mrpresident</p>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <p className="mb-0">User: therealhat</p><p className="mb-0"> Pass: tophat</p>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <p className="mb-0">User: noone</p><p className="mb-0"> Pass: arya</p>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card.Body>
+          </Card>
+        </Row>
+      </Container>
     </div>
   );
 };
